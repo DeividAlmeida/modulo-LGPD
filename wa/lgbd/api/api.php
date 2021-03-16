2609c0818675ee5d957def2531b985c3510561b3
DeividAlmeida <mr.davialmeida@hotmail.com>
2021-03-16 16:40:17 -0300

    api

---
+++ wa/lgbd/api/api.php
@@ -0,0 +1,12 @@
+<?php 
+header('Access-Control-Allow-Origin: *');
+require_once('../../../includes/funcoes.php');
+require_once('../../../database/config.database.php');
+require_once('../../../database/config.php');
+$id = $_GET['id'];
+$codigo = $_GET['codigo'];
+$banco = DBRead('lgbd_categoria','*' ,"WHERE id = '{$id}'")[0];
+$ver = json_decode($banco['analitycs'],true);
+echo $ver[$codigo]['codigo']
+?>
+
